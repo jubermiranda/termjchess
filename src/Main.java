d9271d0942ab86da2e9f15301ee98c3806d50ef9
uniapp <uniapp@uniapp>
2025-04-16 15:23:57 -0400

    initial assets

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    // Peças Brancas
+    System.out.println("White King: \u2654"); // ♔
+    System.out.println("White Queen: \u2655"); // ♕
+    System.out.println("White Rook: \u2656"); // ♖
+    System.out.println("White Bishop: \u2657"); // ♗
+    System.out.println("White Knight: \u2658"); // ♘
+    System.out.println("White Pawn: \u2659"); // ♙
+
+    // Peças Pretas
+    System.out.println("Black King: \u265A"); // ♚
+    System.out.println("Black Queen: \u265B"); // ♛
+    System.out.println("Black Rook: \u265C"); // ♜
+    System.out.println("Black Bishop: \u265D"); // ♝
+    System.out.println("Black Knight: \u265E"); // ♞
+    System.out.println("Black Pawn: \u265F"); // ♟
+  }
+}
---
+++ src/model/ModelExemple.java
@@ -0,0 +1,5 @@
+package model;
+
+public class ModelExemple {
+    // Example model class
+}
