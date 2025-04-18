95b92ada482c4d406898a52d3b4856640ad636f4
uniapp <uniapp@uniapp>
2025-04-18 17:07:03 -0400

    dst != src wbishop

--- src/main/java/com/juber/termjchess/model/piece/WBishop.java
+++ src/main/java/com/juber/termjchess/model/piece/WBishop.java
@@ -3,33 +3,33 @@ package com.juber.termjchess.model.piece;
 import com.juber.termjchess.model.board.BaseCell;
 import com.juber.termjchess.exception.IllegalChessMovementException;
 
-public class WBishop extends BasePiece{
+public class WBishop extends BasePiece {
   public WBishop(BaseCell pos) {
     this.position = pos;
   }
 
   @Override
-  public boolean canMoveTo (BaseCell dst){
-    return this.position.isSameDiagonal(dst);
+  public boolean canMoveTo(BaseCell dst) {
+    return (!this.position.isEquals(dst)) &&
+        this.position.isSameDiagonal(dst);
   }
 
   @Override
-  public void moveTo(BaseCell dst) throws IllegalChessMovementException{
-    if(this.canMoveTo(dst))
+  public void moveTo(BaseCell dst) throws IllegalChessMovementException {
+    if (this.canMoveTo(dst))
       this.position = dst;
     else
       throw new IllegalChessMovementException("cant move to this position");
   }
 
-  @Override 
-  public boolean isW(){
+  @Override
+  public boolean isW() {
     return true;
   }
 
-  @Override 
-  public boolean isB(){
+  @Override
+  public boolean isB() {
     return !this.isW();
   }
 
 }
-
