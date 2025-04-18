fc844792ca9643fb7e5d1d92c5aaa3cff2725713
uniapp <uniapp@uniapp>
2025-04-18 12:32:59 -0400

    add piece tests to script

--- tools/test_compile.sh
+++ tools/test_compile.sh
@@ -1,5 +1,6 @@
 echo "Compilling Tests..."
 javac -d out -sourcepath src/test/java \
     -cp out:lib/junit-platform-console-standalone-1.11.4.jar \
-    src/test/java/com/juber/termjchess/model/board/*Test.java
+    src/test/java/com/juber/termjchess/model/board/*Test.java \
+    src/test/java/com/juber/termjchess/model/piece/*Test.java
 echo "[DONE]"
