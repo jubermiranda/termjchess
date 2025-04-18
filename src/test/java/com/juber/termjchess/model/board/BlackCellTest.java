8171acbb5f920529a1e3c17efdf6b8a6e0e9b4df
uniapp <uniapp@uniapp>
2025-04-18 12:23:50 -0400

    test cell names

--- src/test/java/com/juber/termjchess/model/board/BlackCellTest.java
+++ src/test/java/com/juber/termjchess/model/board/BlackCellTest.java
@@ -43,4 +43,38 @@ public class BlackCellTest {
       shouldThrows  = !shouldThrows;
     }
   }
+
+  // testa o nome das casas
+  // (esta funcao testa apenas alguns)
+  @Test
+  void testCellNames(){
+    try {
+
+      BlackCell bc;
+      WhiteCell wc;
+
+      bc = new BlackCell(0,0);
+      assertEquals("a1", bc.cellName());
+      wc = new WhiteCell(0,7);
+      assertEquals("h1", wc.cellName());
+
+      bc = new BlackCell(1,1);
+      assertEquals("b2", bc.cellName());
+      wc = new WhiteCell(1,6);
+      assertEquals("g2", wc.cellName());
+
+      bc = new BlackCell(2,2);
+      assertEquals("c3", bc.cellName());
+      wc = new WhiteCell(2,5);
+      assertEquals("f3", wc.cellName());
+
+      bc = new BlackCell(3,3);
+      assertEquals("d4", bc.cellName());
+      wc = new WhiteCell(3,4);
+      assertEquals("e4", wc.cellName());
+
+    } catch (InvalidBoardCellPosition e){
+      fail("Invalid cell position");
+    }
+  }
 }
