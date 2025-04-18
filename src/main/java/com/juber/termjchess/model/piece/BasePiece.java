9e47d28f9ae30de450b7425a619ca68f8c3c6e46
uniapp <uniapp@uniapp>
2025-04-18 15:35:50 -0400

    add move function to basePiece

--- src/main/java/com/juber/termjchess/model/piece/BasePiece.java
+++ src/main/java/com/juber/termjchess/model/piece/BasePiece.java
@@ -1,11 +1,14 @@
 package com.juber.termjchess.model.piece;
 
 import com.juber.termjchess.model.board.BaseCell;
+import com.juber.termjchess.model.exception.IllegalChessMovementException;
 
 public abstract class BasePiece {
   protected BaseCell position;
 
   public abstract boolean canMoveTo(BaseCell dst);
+
+  public abstract void moveTo(BaseCell dst) throws IllegalChessMovementException;
 
   public abstract boolean isW();
 
