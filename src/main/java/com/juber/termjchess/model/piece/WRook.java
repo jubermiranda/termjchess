64fb179146ebedc375953f3ea22ce7345b87f804
uniapp <uniapp@uniapp>
2025-04-18 17:04:26 -0400

    add check canMoveTo dst != src

--- src/main/java/com/juber/termjchess/model/piece/WRook.java
+++ src/main/java/com/juber/termjchess/model/piece/WRook.java
@@ -3,33 +3,33 @@ package com.juber.termjchess.model.piece;
 import com.juber.termjchess.model.board.BaseCell;
 import com.juber.termjchess.exception.IllegalChessMovementException;
 
-public class WRook extends BasePiece{
+public class WRook extends BasePiece {
   public WRook(BaseCell pos) {
     this.position = pos;
   }
 
   @Override
-  public boolean canMoveTo (BaseCell dst){
-    return (this.position.isSameCol(dst) || this.position.isSameRow(dst));
+  public boolean canMoveTo(BaseCell dst) {
+    return (!this.position.isEquals(dst)) &&
+        (this.position.isSameCol(dst) || this.position.isSameRow(dst));
   }
 
   @Override
-  public void moveTo(BaseCell dst) throws IllegalChessMovementException{
-    if(this.canMoveTo(dst))
+  public void moveTo(BaseCell dst) throws IllegalChessMovementException {
+    if (this.canMoveTo(dst))
       this.position = dst;
     else
       throw new IllegalChessMovementException("cant move to this position");
   }
 
-  @Override 
-  public boolean isW(){
+  @Override
+  public boolean isW() {
     return true;
   }
 
-  @Override 
-  public boolean isB(){
+  @Override
+  public boolean isB() {
     return !this.isW();
   }
 
 }
-
