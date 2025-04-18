197c71268b1384060e782f29ac0c1b65d5eb2dad
uniapp <uniapp@uniapp>
2025-04-18 13:20:50 -0400

    test white bishop creation

--- src/test/java/com/juber/termjchess/model/piece/WBishopTest.java
+++ src/test/java/com/juber/termjchess/model/piece/WBishopTest.java
@@ -1,22 +1,39 @@
 package com.juber.termjchess.model.piece;
 
+import com.juber.termjchess.exception.InvalidBoardCellPosition;
+import com.juber.termjchess.model.board.BlackCell;
+import com.juber.termjchess.model.board.WhiteCell;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class WBishopTest {
+  BlackCell bCell;
+  WhiteCell wCell;
+
+  @BeforeEach
+  public void setUp() {
+    try {
+      wCell = new WhiteCell(0, 5);
+      bCell = new BlackCell(0, 2);
+    } catch (InvalidBoardCellPosition e) {
+      fail("unexpected error creating bishops cells");
+    }
+  }
+
+  // cria bispo preto nas duas casas iniciais
   @Test
-  void testTemplate() {
-    boolean isImplemented = false;
-    assertTrue(isImplemented);
+  void testIsWB() {
+    WBishop bishop;
+
+    bishop = new WBishop(bCell);
+    assertTrue(bishop.isW());
+    assertFalse(bishop.isB());
+
+    bishop = new WBishop(wCell);
+    assertTrue(bishop.isW());
+    assertFalse(bishop.isB());
   }
 }
-
-
-
-
-
-
-
-
-
 
