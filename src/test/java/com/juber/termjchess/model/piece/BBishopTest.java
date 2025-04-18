4ab7be1d3d3ddfc853b7b54707bcb22c05924f7a
uniapp <uniapp@uniapp>
2025-04-18 13:16:29 -0400

    fix imports, test pass

--- src/test/java/com/juber/termjchess/model/piece/BBishopTest.java
+++ src/test/java/com/juber/termjchess/model/piece/BBishopTest.java
@@ -1,18 +1,38 @@
 package com.juber.termjchess.model.piece;
 
+import com.juber.termjchess.exception.InvalidBoardCellPosition;
+import com.juber.termjchess.model.board.BlackCell;
+import com.juber.termjchess.model.board.WhiteCell;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BBishopTest {
-  BlackCell cell = new BlackCell(7,5);
+  BlackCell bCell;
+  WhiteCell wCell;
 
+  @BeforeEach
+  public void setUp() {
+    try {
+      bCell = new BlackCell(7, 5);
+      wCell = new WhiteCell(7, 2);
+    } catch (InvalidBoardCellPosition e) {
+      fail("unexpected error creating bishops cells");
+    }
+  }
+
+  // cria bispo preto nas duas casas iniciais
   @Test
   void testIsWB() {
-    BBishop bishop = new BBishop(cell);
+    BBishop bishop;
+
+    bishop = new BBishop(bCell);
+    assertTrue(bishop.isB());
+    assertFalse(bishop.isW());
+
+    bishop = new BBishop(wCell);
     assertTrue(bishop.isB());
     assertFalse(bishop.isW());
   }
 }
-
-
-
--- tools/compile.sh
+++ tools/compile.sh
@@ -3,9 +3,7 @@ javac -d out/ -sourcepath src/main/java \
     src/main/java/com/juber/termjchess/*.java \
     src/main/java/com/juber/termjchess/model/board/*.java \
     src/main/java/com/juber/termjchess/model/piece/*.java \
-    src/main/java/com/juber/termjchess/exception/*.java \
-    #src/main/java/com/juber/termjchess/service/*.java \
-    #src/main/java/com/juber/termjchess/util/*.java
+    src/main/java/com/juber/termjchess/exception/*.java 
 echo "[DONE]"
 
 echo "set resources"
