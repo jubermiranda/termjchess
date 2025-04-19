db1b75cd3aed7d31ed6cd2ce138aaeefd99b26c7
uniapp <uniapp@uniapp>
2025-04-18 21:25:19 -0400

    remove bknight redundant test

--- src/test/java/com/juber/termjchess/model/piece/BKnightTest.java
+++ src/test/java/com/juber/termjchess/model/piece/BKnightTest.java
@@ -3,16 +3,22 @@ package com.juber.termjchess.model.piece;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.juber.termjchess.exception.InvalidBoardCellPosition;
+import com.juber.termjchess.model.board.BaseCell;
+import com.juber.termjchess.model.board.WhiteCell;
+
 public class BKnightTest {
   @Test
-  void testTemplate() {
-    boolean isImplemented = false;
-    assertTrue(isImplemented);
+  void testIsWB() {
+    try {
+      BKnight knight = new BKnight(new WhiteCell(3, 4));
+
+      assertTrue(knight.isB());
+      assertFalse(knight.isW());
+
+    } catch (InvalidBoardCellPosition e){
+      fail("unexpected error creating cells");
+    }
   }
 }
-
-
-
-
-
 
