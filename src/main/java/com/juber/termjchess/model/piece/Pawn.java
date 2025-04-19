4ef4cded8171517d934e8ecb7e44859bde8f379f
uniapp <uniapp@uniapp>
2025-04-19 14:03:46 -0400

    add attr firs move to pwan

--- src/main/java/com/juber/termjchess/model/piece/Pawn.java
+++ src/main/java/com/juber/termjchess/model/piece/Pawn.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 import com.juber.termjchess.exception.IllegalChessMovementException;
 
 public abstract class Pawn extends BasePiece{
+  private boolean has_moved;
+
   public Pawn(BaseCell pos) {
     this.position = pos;
+    this.has_moved = false;
   }
 
   @Override
@@ -18,8 +21,11 @@ public abstract class Pawn extends BasePiece{
 
   @Override
   public void moveTo(BaseCell dst) throws IllegalChessMovementException{
-    if(this.canMoveTo(dst))
+    if(this.canMoveTo(dst)){
+      if(!this.has_moved)
+        this.has_moved = true;
       this.position = dst;
+    }
     else
       throw new IllegalChessMovementException("cant move to this position");
   }
