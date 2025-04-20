d73ba821f7f6da457ed58da582fd240447de3b95
uniapp <uniapp@uniapp>
2025-04-19 22:04:53 -0400

    write test wpaen trace. test pass

--- src/test/java/com/juber/termjchess/model/piece/WPawnTest.java
+++ src/test/java/com/juber/termjchess/model/piece/WPawnTest.java
@@ -21,6 +21,20 @@ public class WPawnTest {
       fail("unexpected error creating cells");
     }
   }
+
+  //apenas uma unica ocasiao que trace sera nao vazio:
+  //primeiro movimento do peao, a 2 casas a frente
+  @Test
+  void testPawnTrace(){
+    BaseCell initialPos = BaseCell.createCell("a2");
+    WPawn pawn = new WPawn(initialPos);
+
+    BaseCell newPos = BaseCell.createCell("a3");
+    assertEquals(0, pawn.getTrace(newPos).size());
+
+    newPos = BaseCell.createCell("a4");
+    assertEquals(1, pawn.getTrace(newPos).size());
+  }
 }
 
 
