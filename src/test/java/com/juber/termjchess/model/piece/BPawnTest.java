9b6959afb3ab5062d17f088bfe638d092e96afde
uniapp <uniapp@uniapp>
2025-04-19 22:07:48 -0400

    write bpawn test. test pass

--- src/test/java/com/juber/termjchess/model/piece/BPawnTest.java
+++ src/test/java/com/juber/termjchess/model/piece/BPawnTest.java
@@ -1,6 +1,7 @@
 package com.juber.termjchess.model.piece;
 
 import com.juber.termjchess.exception.InvalidBoardCellPosition;
+import com.juber.termjchess.model.board.BaseCell;
 import com.juber.termjchess.model.board.BlackCell;
 import com.juber.termjchess.model.board.WhiteCell;
 
@@ -19,6 +20,20 @@ public class BPawnTest {
     } catch (InvalidBoardCellPosition e){
       fail("unexpected error creating cells");
     }
+  }
+
+  //apenas uma unica ocasiao que trace sera nao vazio:
+  //primeiro movimento do peao, a 2 casas a frente
+  @Test
+  void testPawnTrace(){
+    BaseCell initialPos = BaseCell.createCell("a7");
+    BPawn pawn = new BPawn(initialPos);
+
+    BaseCell newPos = BaseCell.createCell("a6");
+    assertEquals(0, pawn.getTrace(newPos).size());
+
+    newPos = BaseCell.createCell("a5");
+    assertEquals(1, pawn.getTrace(newPos).size());
   }
 }
 
