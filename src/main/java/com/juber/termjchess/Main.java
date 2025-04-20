c7f5f45dbc4f493d104555b1f6cdf27dbbfdcd0d
uniapp <uniapp@uniapp>
2025-04-20 15:07:55 -0400

    test board custom initial position. (works)

--- src/main/java/com/juber/termjchess/Main.java
+++ src/main/java/com/juber/termjchess/Main.java
@@ -4,10 +4,13 @@ import com.juber.termjchess.service.TermPrinter;
 public class Main {
   public static void main(String[] args) {
     TermPrinter printer = new TermPrinter(4, 0);
-    int n = printer.getBoardSize();
+    int n = printer.getBoardSize() + 4;
     char[][] board = new char[n][n];
-    printer.initBoard(board, 0, 0);
-    printer.printBoard(board, 0, 0);
+    for(int i=0; i < n; i++)
+      for(int j=0; j < n; j++)
+        board[i][j] = ' ';
+
+    printer.printBoard(board, 3, 3);
     System.out.println("Board::::::");
     System.out.println("size = " + Integer.toString(n) );
     for(int i = 0; i < board.length; i++){
